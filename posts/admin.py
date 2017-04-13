3ec17ce62aa00d9f6b82e6c297fab74e729aa7d5
Jesus Noland <jesusnoland@gmail.com>
2017-04-13 07:47:42 +0000

    Adding person model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from .models import Person
 
 # Register your models here.
+
+class PersonInline(admin.StackedInline):
+    model = Person
+    can_delete = False
+    verbose_name_plural = 'people'
+
+# Register
+
+admin.site.register(Person)
+
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 # Create your models here.
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    editor = models.BooleanField(default=False)
+

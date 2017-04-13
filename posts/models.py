5ca617be6734a9db8fe9218530dd43e9cf862739
Jesus Noland <jesusnoland@gmail.com>
2017-04-13 08:55:10 +0000

    Adding Post model

--- posts/models.py
+++ posts/models.py
@@ -8,3 +8,9 @@ class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     editor = models.BooleanField(default=False)
 
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    image = models.FileField(upload_to='uploads/images/')
+    author = models.ForeignKey(Person, on_delete=models.CASCADE)
+

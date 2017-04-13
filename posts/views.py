4648a78764ec9a15a2e245236392f0005eb5f00d
Jesus Noland <jesusnoland@gmail.com>
2017-04-13 08:29:14 +0000

    Logging in of users and view posts index page

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,31 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import auth
 
 # Create your views here.
 
+@csrf_exempt
+def logout(request):
+    auth.logout(request)
+    return redirect( '/posts/login/' )
+
+@csrf_exempt
+def index(request):
+    if request.user.is_authenticated():
+        return render(request, 'index.html', {})
+    return redirect( '/posts/login/' )
+
+@csrf_exempt
 def login(request):
-    return HttpResponse("Login in to see posts")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None and user.is_active:
+            auth.login( request, user )
+            return redirect( '/posts/' )
+
+    return render(request, 'login.html', {})
 
--- quickblog/urls.py
+++ quickblog/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^posts/login', include('posts.urls')),
+    url(r'^posts/', include('posts.urls')),
     url(r'^admin/', admin.site.urls),
 ]

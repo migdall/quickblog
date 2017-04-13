749538bdb33542065cc5aa51a4dd2fdf4f5eafca
Jesus Noland <jesusnoland@gmail.com>
2017-04-13 17:23:11 +0000

    Redo layout to display 3 column layout and editing url route

--- posts/urls.py
+++ posts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^create/$', views.create_post, name='create'),
-    url(r'^(\d)/edit/$', views.edit_post, name='edit'),
-    url(r'^(\d)/delete/$', views.delete_post, name='delete')
+    url(r'^(\d+)/edit/$', views.edit_post, name='edit'),
+    url(r'^(\d+)/delete/$', views.delete_post, name='delete')
 ]
 

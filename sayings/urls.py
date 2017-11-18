0998c2fd683c61ba39f266386a1c2571f1f1afb4
Jesus Noland <jesusnoland@gmail.com>
2017-11-18 16:28:11 +0000

    Saving a Saying's Hero properly and displaying the question.html template

--- sayings/urls.py
+++ sayings/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import new_saying, set_hero
+from .views import new_saying, set_hero, get_question
 
 urlpatterns = [
     url(r'^saying/add/$', new_saying, name='create a new saying'),
-    url(r'^(?P<saying_id>[\W\w]+)/hero/$', set_hero, name='pick a hero for your saying')
+    url(r'^(?P<saying_id>[\W\w]+)/hero/$', set_hero, name='pick a hero for your saying'),
+    url(r'^(?P<saying_id>[\W\w]+)/questions/(?P<question_id>[\d]+)/$', get_question, name='the first question')
 ]
--- sayings/views.py
+++ sayings/views.py
@@ -8,16 +8,31 @@ from .models import Question, Answer, Hero, Saying
 def new_saying(request):
     if request.POST:
         first_name = request.POST.get("firstname")
-        new_saying = Saying.objects.create(first_name=first_name)
-        new_saying.save()
-        return redirect('/sayings/%s/hero/' % str(new_saying.id))
+        if first_name:
+            new_saying = Saying.objects.create(first_name=first_name)
+            new_saying.save()
+            return redirect('/sayings/%s/hero/' % str(new_saying.id))
 
 
 def set_hero(request, saying_id):
+    if request.POST:
+        hero_id = request.POST.get("hero")
+        if hero_id:
+            hero = Hero.objects.get(id=hero_id)
+            saying = Saying.objects.get(id=saying_id)
+            saying.hero = hero
+            saying.save()
+            return redirect('/sayings/%s/questions/1/' % str(saying.id))
     c = {}
     saying = Saying.objects.get(id=saying_id)
     heroes = Hero.objects.all()
     c['saying'] = saying
     c['heroes'] = heroes
     return render(request, 'hero.html', c)
+
+
+def get_question(request, saying_id, question_id):
+    c = {}
+    saying = Saying.objects.get(id=saying_id)
+    return render(request, 'question.html', c)
 

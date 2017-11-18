e0f0c91ee6d3a9764ed78e8123eaa9a835ead592
Jesus Noland <jesusnoland@gmail.com>
2017-11-18 15:17:22 +0000

    Adding sayings models to the Admin

--- sayings/admin.py
+++ sayings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Question, Answer, Hero, Saying
 
 # Register your models here.
+
+
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Hero)
+admin.site.register(Saying)
+
